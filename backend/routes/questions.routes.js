3792c37bca88539d4dde0982fd9ba02289bc7098
Anujahirwar01 <anujahirwar77010@gmail.com>
2025-07-02 15:54:40 +0530

    done

--- backend/routes/questions.routes.js
+++ backend/routes/questions.routes.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import * as questionController from '../controller/questions.controller.js';
 import { body } from 'express-validator';
-import * as authMiddleware from '../middleware/auth.middleware.js';
+import  authMiddleware from '../middleware/auth.middleware.js';
 
 const router = Router();
 
@@ -13,19 +13,19 @@ router.post(
     body('description').notEmpty().withMessage('Description is required'),
     body('tags').isArray().withMessage('Tags required'),
   ],
-  authMiddleware.authMiddleware,
+  authMiddleware ,
   questionController.AskQuestion
 );
 
 router.get("/", questionController.getAllQuestions);
 router.get("/:id", questionController.getQuestionById);
-router.post("/:id/upvote", authMiddleware.authMiddleware, questionController.upvoteQuestion);
-router.post("/:id/answer", authMiddleware.authMiddleware, questionController.postAnswer);
+router.post("/:id/upvote", authMiddleware, questionController.upvoteQuestion);
+router.post("/:id/answer", authMiddleware, questionController.postAnswer);
 
 // ✅ New DELETE route for removing an answer (only by owner)
 router.delete(
   "/:questionId/answer/:answerId",
-  authMiddleware.authMiddleware,
+  authMiddleware,
   questionController.deleteAnswer
 );
 
