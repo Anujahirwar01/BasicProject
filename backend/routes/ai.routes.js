b73e11851205a33d60e1389e106efcfd21d77a14
Anujahirwar01 <anujahirwar77010@gmail.com>
2025-07-06 17:54:06 +0530

    done

--- backend/routes/ai.routes.js
+++ backend/routes/ai.routes.js
@@ -1,6 +1,6 @@
 // routes/ai.routes.js
 import express from "express";
-import { generateAIAnswer } from "../controllers/aiController.js";
+import { generateAIAnswer } from "../controller/ai.controller.js";
 
 const router = express.Router();
 router.post("/generate", generateAIAnswer);
